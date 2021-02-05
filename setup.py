75dcd97776f6a2515b9fd58da9de5c97a769d91b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-02-05 17:05:12 +0000

    Fix typo.

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(name="ognibuild",
         ]
       },
       install_requires=['breezy', 'buildlog-consultant'],
-      test_requires=['python_debian', 'buildlog-consultant', 'breezy'],
+      tests_require=['python_debian', 'buildlog-consultant', 'breezy'],
       test_suite='ognibuild.tests.test_suite',
       )
