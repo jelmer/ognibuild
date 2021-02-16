17b79eb3264eaf3b3d9ad343aa1a0e76ba678cda
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-02-16 23:37:44 +0000

    Missing import.

--- ognibuild/tests/test_debian_fix_build.py
+++ ognibuild/tests/test_debian_fix_build.py
@@ -15,6 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import os
 import re
 
 from debian.deb822 import Deb822
