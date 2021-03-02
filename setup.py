9c74e770a6e5620f9cec1058688250928c1ca8f4
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-02 17:55:13 +0000

    Release 0.0.2.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(name="ognibuild",
       description="Detect and run any build system",
-      version="0.0.1",
+      version="0.0.2",
       maintainer="Jelmer Vernooĳ",
       maintainer_email="jelmer@jelmer.uk",
       license="GNU GPLv2 or later",
