669b84ed8aba0b8e4e48f1edccdbbc52eb812a5c
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-24 17:34:28 +0000

    Handle mvn.

--- ognibuild/debian/file_search.py
+++ ognibuild/debian/file_search.py
@@ -274,6 +274,8 @@ GENERATED_FILE_SEARCHER = GeneratedFileSearcher(
         "/usr/bin/rst2html": "python3-docutils",
         # aclocal is a symlink to aclocal-1.XY
         "/usr/bin/aclocal": "automake",
+        # maven lives in /usr/share
+        "/usr/bin/mvn": "maven",
     }
 )
 
