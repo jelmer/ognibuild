90f740fd4a7173717ce20df78c28b12c91fffa57
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-04-03 00:38:17 +0100

    Handle distmeta not being supported.

--- ognibuild/buildsystem.py
+++ ognibuild/buildsystem.py
@@ -1455,7 +1455,13 @@ class PerlBuildTiny(BuildSystem):
 
     def get_declared_dependencies(self, session, fixers=None):
         self.setup(session, fixers)
-        run_with_build_fixers(session, ["./Build", "distmeta"], fixers)
+        try:
+            run_with_build_fixers(session, ["./Build", "distmeta"], fixers)
+        except UnidentifiedError as e:
+            if "No such action 'distmeta'" in e.lines:
+                pass
+            else:
+                raise
         try:
             with open(os.path.join(self.path, 'META.yml'), 'r') as f:
                 yield from _declared_deps_from_meta_yml(f)
