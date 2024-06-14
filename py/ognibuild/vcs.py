dc5e8bc1c349a59757739c842037af3e47223a22
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-06-14 20:16:49 +0100

    Convert dupe_vcs_tree to rust

--- py/ognibuild/vcs.py
+++ py/ognibuild/vcs.py
@@ -15,50 +15,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-import errno
-
-from breezy.errors import NotBranchError
-from breezy.revisiontree import RevisionTree
-from breezy.tree import Tree
-from breezy.workingtree import WorkingTree
-from buildlog_consultant.sbuild import (
-    NoSpaceOnDevice,
-)
-
-from . import DetailedFailure
 from ._ognibuild_rs import (
+    dupe_vcs_tree,
     export_vcs_tree,
 )
 
 __all__ = [
     "export_vcs_tree",
+    "dupe_vcs_tree",
 ]
-
-
-def dupe_vcs_tree(tree: Tree, directory: str) -> None:
-    with tree.lock_read():
-        if isinstance(tree, WorkingTree):
-            tree = tree.basis_tree()
-    assert isinstance(tree, RevisionTree)
-    try:
-        result = tree._repository.controldir.sprout(
-            directory,
-            create_tree_if_local=True,
-            revision_id=tree.get_revision_id(),
-        )
-    except OSError as e:
-        if e.errno == errno.ENOSPC:
-            raise DetailedFailure(1, ["sprout"], NoSpaceOnDevice()) from e
-        raise
-    if not result.has_workingtree():
-        raise AssertionError
-    # Copy parent location - some scripts need this
-    if isinstance(tree, WorkingTree):
-        parent = tree.branch.get_parent()
-    else:
-        try:
-            parent = tree._repository.controldir.open_branch().get_parent()
-        except NotBranchError:
-            parent = None
-    if parent:
-        result.open_branch().set_parent(parent)
--- src/vcs.rs
+++ src/vcs.rs
@@ -3,6 +3,7 @@ use pyo3::exceptions::PyIOError;
 use pyo3::import_exception;
 use pyo3::prelude::*;
 use std::path::Path;
+use url::Url;
 
 import_exception!(ognibuild, DetailedFailure);
 
@@ -37,4 +38,58 @@ pub fn export_vcs_tree(
         }
         Ok(())
     })
+}
+
+pub trait DupableTree {
+    fn tree(&self) -> breezyshim::tree::RevisionTree;
+
+    fn get_parent(&self) -> Option<String>;
+}
+
+impl DupableTree for &breezyshim::tree::WorkingTree {
+    fn tree(&self) -> breezyshim::tree::RevisionTree {
+        self.basis_tree()
+    }
+
+    fn get_parent(&self) -> Option<String> {
+        self.branch().get_parent()
+    }
+}
+
+impl DupableTree for &breezyshim::tree::RevisionTree {
+    fn tree(&self) -> breezyshim::tree::RevisionTree {
+        self.repository()
+            .revision_tree(&self.get_revision_id())
+            .unwrap()
+    }
+
+    fn get_parent(&self) -> Option<String> {
+        let branch = self.repository().controldir().open_branch(None).unwrap();
+
+        branch.get_parent()
+    }
+}
+
+pub fn dupe_vcs_tree(
+    orig_tree: impl DupableTree,
+    directory: &Path,
+) -> Result<(), breezyshim::controldir::OpenError> {
+    let tree = orig_tree.tree();
+    let result = tree.repository().controldir().sprout(
+        Url::from_directory_path(directory).unwrap(),
+        None,
+        Some(true),
+        None,
+        Some(&tree.get_revision_id()),
+    );
+
+    assert!(result.has_workingtree());
+
+    // Copy parent location - some scripts need this
+    if let Some(parent) = orig_tree.get_parent() {
+        let mut branch = result.open_branch(None).unwrap();
+        branch.set_parent(&parent);
+    }
+
+    Ok(())
 }
