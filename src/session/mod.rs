b35cdc83e474168aa67b31e2259f74570fd9e1f0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-08-04 00:38:43 +0100

    Add PlainSession

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,8 +1,24 @@
-pub mod schroot;
+use std::collections::HashMap;
 
+pub mod schroot;
+pub mod plain;
+
+#[derive(Debug)]
 pub enum Error {
-    CalledProcessError(u8)
+    CalledProcessError(i32),
+    IoError(std::io::Error),
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Error::CalledProcessError(code) => write!(f, "CalledProcessError({})", code),
+            Error::IoError(e) => write!(f, "IoError({})", e),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
 
 pub trait Session {
     fn check_output(
@@ -28,8 +44,25 @@ pub fn which(session: &impl Session, name: &str) -> Option<String> {
     }
 }
 
-pub fn get_user(session: &Session) -> String {
+pub fn get_user(session: &impl Session) -> String {
     String::from_utf8(
-    session.check_output(vec!["sh", "-c", "echo $USER"], Some("/"), None, None).unwrap())
-        .trim()
+    session.check_output(vec!["sh", "-c", "echo $USER"], Some("/"), None, None).unwrap()).unwrap()
+        .trim().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_get_user() {
+        let session = super::plain::PlainSession::new();
+        let user = super::get_user(&session);
+        assert!(user.len() > 0);
+    }
+
+    #[test]
+    fn test_which() {
+        let session = super::plain::PlainSession::new();
+        let which = super::which(&session, "ls");
+        assert_eq!(which.unwrap().ends_with("/ls"), true);
+    }
 }
---
+++ src/session/plain.rs
@@ -0,0 +1,54 @@
+pub struct PlainSession;
+
+impl PlainSession {
+    pub fn new() -> Self {
+        PlainSession
+    }
+
+    fn prepend_user<'a>(&'a self, user: Option<&'a str>, mut args: Vec<&'a str>) -> Vec<&'a str> {
+        if let Some(user) = user {
+            if user != whoami::username() {
+                args = vec!["sudo", "-u", user].into_iter().chain(args).collect();
+            }
+        }
+        args
+    }
+}
+
+impl crate::session::Session for PlainSession {
+    fn check_output(
+        &self,
+        argv: Vec<&str>,
+        cwd: Option<&str>,
+        user: Option<&str>,
+        env: Option<std::collections::HashMap<String, String>>,
+    ) -> Result<Vec<u8>, crate::session::Error> {
+        let argv = self.prepend_user(user, argv);
+        let mut binding = std::process::Command::new(argv[0]);
+        let mut cmd = binding
+            .args(&argv[1..]);
+
+        if let Some(cwd) = cwd {
+            cmd = cmd.current_dir(cwd);
+        }
+
+        if let Some(env) = env {
+            cmd = cmd.envs(env);
+        }
+
+        let output = cmd.output();
+
+        match output {
+            Ok(output) => {
+                if output.status.success() {
+                    Ok(output.stdout)
+                } else {
+                    Err(crate::session::Error::CalledProcessError(
+                        output.status.code().unwrap(),
+                    ))
+                }
+            }
+            Err(e) => Err(crate::session::Error::IoError(e)),
+        }
+    }
+}
