4d1193fb741eb039180fb2bc85ba97f2fce31095
Jelmer Vernooij <jelmer@jelmer.uk>
2024-08-03 23:40:57 +0000

    Add tests

--- src/session/schroot.rs
+++ src/session/schroot.rs
@@ -18,3 +18,23 @@ pub fn generate_session_id(prefix: &str) -> String {
     .unwrap();
     format!("{}-{}", sanitize_session_name(prefix), suffix)
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_sanitize_session_name() {
+        assert_eq!(super::sanitize_session_name("foo"), "foo");
+        assert_eq!(super::sanitize_session_name("foo-bar"), "foo-bar");
+        assert_eq!(super::sanitize_session_name("foo_bar"), "foo_bar");
+        assert_eq!(super::sanitize_session_name("foo.bar"), "foo.bar");
+        assert_eq!(super::sanitize_session_name("foo!bar"), "foobar");
+        assert_eq!(super::sanitize_session_name("foo@bar"), "foobar");
+    }
+
+    #[test]
+    fn test_generate_session_id() {
+        let id = super::generate_session_id("foo");
+        assert_eq!(id.len(), 12);
+        assert_eq!(&id[..4], "foo-");
+    }
+}
