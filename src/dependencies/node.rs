222b9ad778bf007cecba786f97b0518b84af2e04
Jelmer Vernooij <jelmer@jelmer.uk>
2024-08-24 12:06:37 +0000

    Migrate NpmResolvre

--- src/dependencies/node.rs
+++ src/dependencies/node.rs
@@ -1,4 +1,5 @@
-use crate::dependency::Dependency;
+use crate::dependencies::BinaryDependency;
+use crate::dependency::{Installer, Error, Explanation, Dependency};
 use crate::session::Session;
 use serde::{Deserialize, Serialize};
 
@@ -80,5 +81,103 @@ impl Dependency for NodeModuleDependency {
     }
     fn as_any(&self) -> &dyn std::any::Any {
         self
+    }
+}
+
+    fn command_package(command: &str) -> Option<&str> {
+        match command {
+            "del-cli" => Some("del-cli"),
+            "husky" => Some("husky"),
+            "cross-env" => Some("cross-env"),
+            "xo" => Some("xo"),
+            "standard" => Some("standard"),
+            "jshint" => Some("jshint"),
+            "if-node-version" => Some("if-node-version"),
+            "babel-cli" => Some("babel"),
+            "c8" => Some("c8"),
+            "prettier-standard" => Some("prettier-standard"),
+            _ => None,
+        }
+    }
+
+pub struct NpmResolver {
+    session: Box<dyn Session>,
+    user_local: bool,
+}
+
+impl NpmResolver {
+    pub fn new(session: Box<dyn Session>, user_local: bool) -> Self {
+        Self { session, user_local }
+    }
+
+    fn cmd(&self, reqs: &[&NodePackageDependency]) -> Vec<String> {
+        let mut cmd = vec!["npm".to_string(), "install".to_string()];
+        if !self.user_local {
+            cmd.push("-g".to_string());
+        }
+        cmd.extend(reqs.iter().map(|req| req.package.clone()));
+        cmd
+    }
+
+}
+
+impl From<NodeModuleDependency> for NodePackageDependency {
+    fn from(dep: NodeModuleDependency) -> Self {
+        let parts: Vec<&str> = dep.module.split('/').collect();
+        Self {
+            // TODO: Is this legit?
+            package: if parts[0].starts_with('@') {
+                parts[..2].join("/")
+            } else {
+                parts[0].to_string()
+            }
+        }
+    }
+}
+
+fn to_node_package_req(requirement: &dyn Dependency) -> Option<NodePackageDependency> {
+    if let Some(requirement) = requirement.as_any().downcast_ref::<NodeModuleDependency>() {
+        Some(requirement.clone().into())
+    } else if let Some(requirement) = requirement.as_any().downcast_ref::<NodePackageDependency>() {
+        Some(requirement.clone())
+    } else if let Some(requirement) = requirement.as_any().downcast_ref::<BinaryDependency>() {
+        command_package(&requirement.binary_name).map(NodePackageDependency::new)
+    } else {
+        None
+    }
+}
+
+impl Installer for NpmResolver {
+    fn explain(&self, requirement: &dyn Dependency) -> Result<Explanation, Error> {
+        let requirement = requirement.as_any()
+            .downcast_ref::<NodePackageDependency>()
+            .ok_or(Error::UnknownDependencyFamily)?;
+
+        Ok(Explanation {
+            message: format!("install node package {}", requirement.package),
+            command: Some(self.cmd(&[requirement])),
+        })
+    }
+
+    fn install(&self, requirement: &dyn Dependency) -> Result<(), Error> {
+        let requirement = requirement.as_any()
+            .downcast_ref::<NodePackageDependency>()
+            .ok_or(Error::UnknownDependencyFamily)?;
+
+        let cmd = &self.cmd(&[requirement]);
+        crate::analyze::run_detecting_problems(
+            self.session.as_ref(),
+            cmd.iter().map(|s| s.as_str()).collect(),
+            None,
+            false,
+            None,
+            if self.user_local { None } else { Some("root") },
+            None,
+            None,
+            None,
+            None
+        )?;
+
+        Ok(())
     }
 }
