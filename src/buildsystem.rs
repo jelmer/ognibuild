97addf40d45f9a04099097c8e39d1e4427ae6038
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-08-26 15:08:24 +0000

    Add guaranteed_which in rust

--- src/buildsystem.rs
+++ src/buildsystem.rs
@@ -1,3 +1,8 @@
+use crate::dependencies::BinaryDependency;
+use crate::dependency::{Error, InstallationScope, Installer};
+use crate::session::{which, Session};
+use std::path::PathBuf;
+
 /// The category of a dependency
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum DependencyCategory {
@@ -5,7 +10,7 @@ pub enum DependencyCategory {
     Universal,
     /// Building of artefacts
     Build,
-    /// For running artefacts after build or install 
+    /// For running artefacts after build or install
     Runtime,
     /// Test infrastructure, e.g. test frameworks or test runners
     Test,
@@ -18,5 +23,48 @@ pub enum Action {
     Clean,
     Build,
     Test,
-    Install
+    Install,
+}
+
+/// Determine the path to a binary, installing it if necessary
+pub fn guaranteed_which(
+    session: &dyn Session,
+    installer: &dyn Installer,
+    name: &str,
+) -> Result<PathBuf, Error> {
+    match which(session, name) {
+        Some(path) => Ok(PathBuf::from(path)),
+        None => {
+            installer.install(&BinaryDependency::new(name), InstallationScope::Global)?;
+            Ok(PathBuf::from(which(session, name).unwrap()))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::dependency::{Error, NullInstaller};
+    use crate::session::plain::PlainSession;
+    use crate::session::Session;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_guaranteed_which() {
+        let session = PlainSession::new();
+        let installer = NullInstaller::new();
+
+        let _path = guaranteed_which(&session, &installer, "ls").unwrap();
+    }
+
+    #[test]
+    fn test_guaranteed_which_not_found() {
+        let session = PlainSession::new();
+        let installer = NullInstaller::new();
+
+        assert!(matches!(
+            guaranteed_which(&session, &installer, "this-does-not-exist").unwrap_err(),
+            Error::UnknownDependencyFamily,
+        ));
+    }
 }
