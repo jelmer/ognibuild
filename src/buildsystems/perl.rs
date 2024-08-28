048a3e5e410b46cdb50a07cd00e1ee11ff55716f
Jelmer Vernooij <jelmer@jelmer.uk>
2024-08-28 12:48:03 +0000

    Convert DistZilla to rust

--- src/buildsystems/perl.rs
+++ src/buildsystems/perl.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
 use std::io::Read;
+use crate::buildsystem::{guaranteed_which, BuildSystem};
 use crate::session::Session;
-use crate::fix_build::{BuildFixer};
+use crate::fix_build::BuildFixer;
 use crate::installer::Error as InstallerError;
 use crate::dependencies::perl::PerlModuleDependency;
 use crate::buildsystem::DependencyCategory;
+use std::path::{Path,PathBuf};
 
 fn read_cpanfile(session: &dyn Session, args: Vec<&str>, category: DependencyCategory, fixers: &[&dyn BuildFixer<InstallerError>]) -> impl Iterator<Item = (DependencyCategory, PerlModuleDependency)> {
     let mut argv = vec!["cpanfile-dump"];
@@ -65,4 +67,129 @@ pub fn declared_deps_from_meta_yml<R: Read>(f: R) -> Vec<(DependencyCategory, Pe
     }
     // TODO(jelmer): recommends
     ret
+}
+
+pub struct DistZilla {
+    path: PathBuf,
+    dist_inkt_class: Option<String>,
+}
+
+impl DistZilla {
+    pub fn new(path: PathBuf) -> Self {
+        let mut dist_inkt_class = None;
+        let mut f = std::fs::File::open(&path).unwrap();
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).unwrap();
+        for line in contents.lines() {
+            let rest = if let Some(rest) = line.strip_prefix(";;") {
+                rest
+            } else {
+                continue;
+            };
+            let (key, value) = if let Some((key, value)) = rest.split_once('=') {
+                (key.trim(), value.trim())
+            } else {
+                continue;
+            };
+            if key == "class" && value.starts_with("'Dist::Inkt") {
+                dist_inkt_class = Some(value[1..value.len()-1].to_string());
+                break;
+            }
+        }
+        Self {
+            path,
+            dist_inkt_class,
+        }
+    }
+
+    pub fn setup(&self, installer: &dyn crate::installer::Installer) -> Result<(), crate::installer::Error> {
+        let dep = crate::dependencies::perl::PerlModuleDependency::simple("Dist::Inkt");
+        installer.install(&dep, crate::installer::InstallationScope::Global)?;
+        Ok(())
+    }
+
+    pub fn probe(path: &Path) -> Option<Box<dyn BuildSystem>> {
+        let dist_ini_path = path.join("dist.ini");
+        if dist_ini_path.exists() && !path.join("Makefile.PL").exists() {
+            Some(Box::new(Self::new(dist_ini_path)))
+        } else {
+            None
+        }
+    }
+}
+
+impl BuildSystem for DistZilla {
+    fn name(&self) -> &str {
+        "Dist::Zilla"
+    }
+
+    fn dist(
+        &self,
+        session: &dyn Session,
+        installer: &dyn crate::installer::Installer,
+        target_directory: &Path,
+        quiet: bool,
+    ) -> Result<std::ffi::OsString, crate::buildsystem::Error> {
+        self.setup(installer)?;
+        let dc = crate::dist_catcher::DistCatcher::default(&session.external_path(Path::new(".")));
+        if self.dist_inkt_class.is_some() {
+            session.command(vec![guaranteed_which(session, installer, "distinkt-dist").unwrap().to_str().unwrap()]).quiet(quiet).run_detecting_problems()?;
+        } else {
+            // Default to invoking Dist::Zilla
+            session.command(vec![guaranteed_which(session, installer, "dzil").unwrap().to_str().unwrap(), "build", "--tgz"]).quiet(quiet).run_detecting_problems()?;
+        }
+        Ok(dc.copy_single(target_directory).unwrap().unwrap())
+    }
+
+    fn test(&self, session: &dyn Session, installer: &dyn crate::installer::Installer) -> Result<(), crate::buildsystem::Error> {
+        // see also https://perlmaven.com/how-to-run-the-tests-of-a-typical-perl-module
+        self.setup(installer)?;
+        session.command(vec![guaranteed_which(session, installer, "dzil").unwrap().to_str().unwrap(), "test"]).run_detecting_problems()?;
+        Ok(())
+    }
+
+    fn build(&self, session: &dyn Session, installer: &dyn crate::installer::Installer) -> Result<(), crate::buildsystem::Error> {
+        self.setup(installer)?;
+        session.command(vec![guaranteed_which(session, installer, "dzil").unwrap().to_str().unwrap(), "build"]).run_detecting_problems()?;
+        Ok(())
+    }
+
+    fn clean(&self, session: &dyn Session, installer: &dyn crate::installer::Installer) -> Result<(), crate::buildsystem::Error> {
+        todo!()
+    }
+
+    fn install(
+        &self,
+        session: &dyn Session,
+        installer: &dyn crate::installer::Installer,
+        install_target: &crate::buildsystem::InstallTarget
+    ) -> Result<(), crate::buildsystem::Error> {
+        todo!()
+    }
+
+    fn get_declared_dependencies(
+        &self,
+        session: &dyn Session,
+        fixers: Option<&[&dyn crate::fix_build::BuildFixer<InstallerError>]>,
+    ) -> Result<Vec<(DependencyCategory, Box<dyn crate::dependency::Dependency>)>, crate::buildsystem::Error> {
+        let mut ret = vec![];
+        if self.path.exists() {
+            let lines = session.command(vec!["dzil", "authordeps"]).run_fixing_problems(fixers.unwrap_or(&[])).unwrap();
+            for entry in lines {
+                ret.push((DependencyCategory::Build, Box::new(PerlModuleDependency::simple(entry.trim())) as Box<dyn crate::dependency::Dependency>));
+            }
+        }
+        if self.path.parent().unwrap().join("cpanfile").exists() {
+            ret.extend(declared_deps_from_cpanfile(session, fixers.unwrap_or(&[])).into_iter().map(|(category, dep)| (category, Box::new(dep) as Box<dyn crate::dependency::Dependency>)));
+        }
+        Ok(ret)
+    }
+
+    fn get_declared_outputs(
+        &self,
+        session: &dyn Session,
+        fixers: Option<&[&dyn crate::fix_build::BuildFixer<InstallerError>]>,
+    ) -> Result<Vec<Box<dyn crate::output::Output>>, crate::buildsystem::Error> {
+        todo!()
+    }
 }
