37cf6da53dafda6559e215ab5e09a9502aeeb7f4
Jelmer Vernooij <jelmer@jelmer.uk>
2024-10-07 18:22:12 +0000

    Add some more docstrings

--- src/upstream.rs
+++ src/upstream.rs
@@ -1,7 +1,11 @@
+//! This module provides a trait for dependencies that can find their upstream metadata.
 use crate::dependency::Dependency;
 pub use upstream_ontologist::UpstreamMetadata;
 
+/// A trait for dependencies that can find their upstream metadata.
 pub trait FindUpstream: Dependency {
+
+    /// Find the upstream metadata for this dependency.
     fn find_upstream(&self) -> Option<UpstreamMetadata>;
 }
 
